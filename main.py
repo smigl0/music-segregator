bc9e8dc9b0fcb343a32a35ca7c3dc02ebadaa7f4
smigl0 <kamil.smigielski@outlook.com>
2024-05-24 12:31:09 +0200

    added file moving funcionality

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 #!venv/Scripts/python
 
 import sys,os,subprocess
+import json
+from pprint import pprint 
 
 __dirname = os.getcwd()
+
+__outdir = "./out"
 
 __ffprobe_path = os.path.join(__dirname,'bin','ffprobe.exe')
 __ffmpeg_path  = os.path.join(__dirname,'bin','ffmpeg.exe')
 
 __ffprobe_command = ''
 
-if sys.platform == "windows":
+print(sys.platform)
+
+if sys.platform == "win32":
     __ffprobe_command = __ffprobe_path
 elif sys.platform == "linux":
     __ffprobe_command = 'ffprobe'
 
 print(len(sys.argv))
+
+def urlify(urlString: str):
+    print(urlString)
+    urlString = urlString.replace(" ","_")
+    print(urlString)
+    return urlString
 
 if(len(sys.argv)==1):
     raise KeyError('Please pass a path to segregate')
@@ -22,7 +34,16 @@ else:
     __music_path = sys.argv[len(sys.argv)-1]
 
 for subdir,dirs,files in os.walk(__music_path):
-    # print(subdir)
     for file in files:
-        print(os.path.join(subdir,file))
-        print(subprocess.getoutput(f"{__ffprobe_command} \"{os.path.join(subdir,file)}\"") )
+        # print(f"{__ffprobe_command} \"{os.path.join(subdir,file)}\"")
+        fileMetadata = json.loads(subprocess.getoutput(f"{__ffprobe_command} -v quiet -print_format json -show_format -show_streams \"{os.path.join(subdir,file)}\""))['format']['tags']
+
+        __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['album_artist'])),urlify(fileMetadata['album']))
+
+        if not(os.path.exists(__album_dir) and os.path.isdir(__album_dir)):
+            os.makedirs(__album_dir)
+        
+        os.rename(os.path.join(subdir,file),os.path.join(__album_dir,file))
+
+
+        # print(f'{urlify(fileMetadata['album_artist'])}/{urlify(fileMetadata['album'])}')
