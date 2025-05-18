50ab61fc72e7d3fce77900a7b1035041e6ffdd90
smigl0 <kamil.smigielski@outlook.com>
2025-05-18 12:23:25 +0200

    potential update fix

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 
 import sys,os,subprocess
 import json
-from pprint import pprint 
 
 __dirname = os.getcwd()
 
 __outdir = "./out"
 
 __ffprobe_path = os.path.join(__dirname,'bin','ffprobe.exe')
-__ffmpeg_path  = os.path.join(__dirname,'bin','ffmpeg.exe')
 
 __ffprobe_command = ''
 
@@ -35,15 +33,12 @@ else:
 
 for subdir,dirs,files in os.walk(__music_path):
     for file in files:
-        # print(f"{__ffprobe_command} \"{os.path.join(subdir,file)}\"")
-        fileMetadata = json.loads(subprocess.getoutput(f"{__ffprobe_command} -v quiet -print_format json -show_format -show_streams \"{os.path.join(subdir,file)}\""))['format']['tags']
-
-        __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['album_artist'])),urlify(fileMetadata['album']))
-
-        if not(os.path.exists(__album_dir) and os.path.isdir(__album_dir)):
-            os.makedirs(__album_dir)
-        
-        os.rename(os.path.join(subdir,file),os.path.join(__album_dir,file))
-
-
-        # print(f'{urlify(fileMetadata['album_artist'])}/{urlify(fileMetadata['album'])}')
+        if len(subdir.split('\\')) == 1:
+            fileMetadata = json.loads(subprocess.getoutput(f"{__ffprobe_command} -v quiet -print_format json -show_format -show_streams \"{os.path.join(subdir,file)}\""))['format']['tags']
+    
+            __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['album_artist'])),urlify(fileMetadata['album']))
+    
+            if not(os.path.exists(__album_dir) and os.path.isdir(__album_dir)):
+                os.makedirs(__album_dir)
+            
+            os.rename(os.path.join(subdir,file),os.path.join(__album_dir,file))
