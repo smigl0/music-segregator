d5fe883c4069f87137935e82a49d527e243f67ea
smigl0 <kamil.smigielski@outlook.com>
2025-05-18 17:43:18 +0200

    i rewrote the whole thing

--- main.py
+++ main.py
@@ -12,37 +12,36 @@ __ffprobe_path = os.path.join(__dirname,'bin','ffprobe.exe')
 
 __ffprobe_command = ''
 
-print(sys.platform)
-
 if sys.platform == "win32":
     __ffprobe_command = __ffprobe_path
 elif sys.platform == "linux":
     __ffprobe_command = 'ffprobe'
 
-print(len(sys.argv))
-
 def urlify(urlString: str):
-    print(urlString)
-    urlString = urlString.replace(" ","_")
-    print(urlString)
+    urlString = urlString.replace(" ","\\ ")
     return urlString
 
-if(len(sys.argv)==1):
+if(len(sys.argv)<3):
     raise KeyError('Please pass a path to segregate')
 else:
-    __music_path = sys.argv[len(sys.argv)-1]
+    __music_path = sys.argv[1]
+    __outdir = sys.argv[2]
 
-for subdir,dirs,files in os.walk(__music_path):
-    for file in files:
-        if len(subdir.split('\\')) == 1:
+
+if __name__ == '__main__':
+    for file in os.listdir(__music_path):
+        if os.path.isfile(os.path.join(__music_path,file)):
+            cmd = f"{__ffprobe_command} -v quiet -print_format json -show_format -show_streams \"{os.path.join(__music_path,file)}\""
+            fileMetadata=json.loads(subprocess.run(cmd,capture_output=True, text=True).stdout)['format']['tags'] 
             try:
-                fileMetadata = json.loads(subprocess.getoutput(f"{__ffprobe_command} -v quiet -print_format json -show_format -show_streams \"{os.path.join(subdir,file)}\""))['format']['tags']
-    
                 __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['album_artist'])),urlify(fileMetadata['album']))
-    
+            except KeyError:
+                __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['artist'])),urlify(fileMetadata['album']))
+
+            try:
                 if not(os.path.exists(__album_dir) and os.path.isdir(__album_dir)):
                     os.makedirs(__album_dir)
-                
-                shutil.move(os.path.join(subdir,file),os.path.join(__album_dir,file))
-            except KeyError:
+                       
+                shutil.move(os.path.join(__music_path,file),os.path.join(__album_dir,file))
+            except:
                 pass
