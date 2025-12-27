763d4ca1897f5cc98b8cfa07950157bf8a615f90
smigl0 <kamil.smigielski@outlook.com>
2025-12-27 13:38:28 +0100

    minor change to argument passing

--- main.py
+++ main.py
@@ -22,11 +22,16 @@ def urlify(urlString: str):
     #urlString = urlString.replace(" ","\\ ")
     return urlString
 
-if(len(sys.argv)<3):
+if(len(sys.argv)<2):
     raise KeyError('Please pass a path to segregate')
 else:
     __music_path = sys.argv[1]
-    __outdir = sys.argv[2]
+    if(len(sys.argv)==2):
+        __outdir = sys.argv[1]
+    else:
+        __outdir = sys.argv[2]
+
+
 
 
 if __name__ == '__main__':
@@ -35,23 +40,25 @@ if __name__ == '__main__':
         if os.path.isfile(os.path.join(__music_path,file)) and file.split('.')[-1] in ["mp3"]: 
             print(file)         
             cmd = [
-            __ffprobe_command,
-            "-v", "quiet",
-            "-print_format", "json",
-            "-show_format",
-            "-show_streams",os.path.join(__music_path, file)]
-    
+                    __ffprobe_command,
+                    "-v", "quiet",
+                    "-print_format", "json",
+                    "-show_format",
+                    "-show_streams",os.path.join(__music_path, file)]
+
             fileMetadata=json.loads(subprocess.run(cmd,capture_output=True, text=True).stdout)['format']['tags'] 
             print(fileMetadata) 
             try:
                 __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['album_artist'])),urlify(fileMetadata['album']))
             except KeyError:
-                __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['artist'])),urlify(fileMetadata['album']))
-
+                try:
+                    __album_dir = os.path.join(os.path.join(__outdir,urlify(fileMetadata['artist'])),urlify(fileMetadata['album']))
+                except:
+                    pass
             try:
                 if not(os.path.exists(__album_dir) and os.path.isdir(__album_dir)):
                     os.makedirs(__album_dir)
-                       
+
                 shutil.move(os.path.join(__music_path,file),os.path.join(__album_dir,file))
             except:
                 pass
